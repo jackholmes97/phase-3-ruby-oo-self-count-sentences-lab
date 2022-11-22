b5a6a53b37e533204ac0d196a66f3738bc59070b
Jack Holmes <jdh97@comcast.net>
2022-11-22 13:05:49 -0500

    commit tree ed373854e9f95c17fec16f7c5a16c79aa6289823

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = [".", "!", "?"]
+    count = self.split(Regexp.union(delimiters)).filter { |chunk| !chunk.empty?}.count
+    return count
   end
 end
